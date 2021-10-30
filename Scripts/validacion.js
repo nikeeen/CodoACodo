b4f35579bfae763610f87855e29bbb7e4c48ef34
nikeen <maganicolas@gmail.com>
2021-10-29 21:36:01 -0300

    Entrega final

--- Scripts/validacion.js
+++ Scripts/validacion.js
@@ -1,10 +1,3 @@
-// nombres: nombre
-// apellido: apellido
-// dirección: direccion
-// altura: numeropuerta
-// piso: piso
-// numero de DNI: dni
-
 const nombre = document.getElementById("nombre")
 const apellido = document.getElementById("apellido")
 const direccion = document.getElementById("direccion")
@@ -20,12 +13,32 @@ form.addEventListener("submit", e=> {
     let entrar = false
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        alertas += `El nombre no es valido <br>` 
+    if(nombre.value.length <4){
+        alertas += `- El nombre no es válido, tiene que tener más de 3 caracteres <br>` 
+        entrar=true
+    }
+    if(apellido.value.length <4){
+        alertas += `- El apellido no es válido, tiene que tener más de 3 caracteres <br>` 
+        entrar=true
+    }
+    if(direccion.value.length <4){
+        alertas += `- La dirección no es válida, tiene que tener más de 3 caracteres <br>` 
+        entrar=true
+    }
+    if(numeropuerta.value <0){
+        alertas += `- La altura no es correcta, debe indicar un número positivo <br>` 
         entrar=true
     }
     if(!regexEmail.test(mail.value)){
-        alertas += `El email no es valido <br>` 
+        alertas += `- El email no es valido <br>` 
+        entrar=true
+    }
+    if(dni.value.length <7){
+        alertas += `- El DNI debe tener más de 7 dígitos <br>` 
+        entrar=true
+    }
+    if(piso.value.length >3){
+        alertas += `- El piso es incorrecto<br>` 
         entrar=true
     }
     if(entrar){
@@ -34,4 +47,5 @@ form.addEventListener("submit", e=> {
         parrafo.innerHTML = "Enviado"
     }
 })
+
 
